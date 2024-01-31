30ae12eeb46890604c8f868deca0040912a8fc41
feinsero <bruno_queiroz_96@hotmail.com>
2024-01-31 16:59:49 -0300

    Criação do projeto para o desenvolvimento de uma API para listas de reprodução utilizando Spring Boot

---
+++ src/test/java/com/brunoqueiroz/testedev/TestedevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunoqueiroz.testedev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestedevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
